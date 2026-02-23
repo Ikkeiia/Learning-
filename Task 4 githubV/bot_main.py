5c21f21a255c461a8067fa9f81e79a3584b03bd9
ikkeiia <karelstanek8@gmail.com>
2026-02-23 23:26:14 +0100

    Day 15.1

--- Task 4 githubV/bot_main.py
+++ Task 4 githubV/bot_main.py
@@ -1,11 +1,9 @@
-# This example requires the 'message_content' intent.
 import discord
 from discord.ext import commands
 import requests
 
-
+#importing tokens for mal and dc bot
 from tokens import mal_token, discord_token
-
 
 
 description = 'MAL bot'
@@ -22,8 +20,7 @@ def top_rankings():
     """
     (Top 5 anime overall)
     """
-    #url to the request in this case i request by anime id
-
+    #url to the request in this case request for the top 5 list
     url = f'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=5?'
    
     #headers need to be setup for authentification
@@ -32,9 +29,6 @@ def top_rankings():
     }
     #storing the response inside a variable
     response = requests.get(url, headers=headers)
-
-
-    
 
     if response.status_code == 200:
         #for debuging
@@ -50,20 +44,16 @@ def top_rankings():
                 "title": node.get('title'),
                 "image_url": node.get('main_picture').get('large')
             })
-
         return results
     
     else:
         return f"Error: {response.status_code}, {response.text}"
     
 
-
-
 def top_anime_from_season(year: int, season:str):
     """("Enter your year in the format YYYY")   
     ("enter your season:  summer, spring, fall, winter")"""
-    #url to the request in this case i request by anime id
-
+    #url to the request
     url = f"https://api.myanimelist.net/v2/anime/season/{year}/{season}?limit=1"
    
     #headers need to be setup for authentification
@@ -72,8 +62,6 @@ def top_anime_from_season(year: int, season:str):
     }
     #storing the response inside a variable
     response = requests.get(url, headers=headers)
-
-
 
     if response.status_code == 200:
         #for debuging
@@ -90,7 +78,7 @@ def top_anime_from_season(year: int, season:str):
 
 
 """
-The Bot part
+The Bot activity
 """
 
 
